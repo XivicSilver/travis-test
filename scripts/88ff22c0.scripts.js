b93f1861b1b337771b43e73ab9a57bae81ec0d8a
Nicolae Olariu <nicolae-olariu@xivic.com>
2014-01-28 09:27:49 +0000

    adding build files to specific branch

--- scripts/88ff22c0.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("travisTestApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("travisTestApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"],console.log("main controller!")}]);
