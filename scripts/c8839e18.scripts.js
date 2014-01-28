9ea8b4c57fa1ba13df9bf1d5db4091a2114c7e11
xivic <git@xivic.com>
2014-01-28 16:43:42 +0000

    [travis deploy] adding build files to specific branch

---
+++ scripts/c8839e18.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("travisTestApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("travisTestApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"],console.log("main controller test!")}]);
