4748f62c55ca263f42f8a0c84458fa4a3b30928b
Nicolae Olariu <nicolae.olariu@xivic.com>
2014-02-10 15:36:25 +0200

    deploy.sh script updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,22 +11,10 @@ cp -R ../${DESTINATION_FOLDER}/* .
 # Copy all hidden files from root.
 cp ../${DESTINATION_FOLDER}/.* .
 
-#echo "bower_components" >> .gitignore
-# pop the .circle.yml file from the stash
-#echo -e "branches:\n  - only:\n    - master" > .circle.yml
-
-echo "=============================GIT CONFIG=================================="
-
 git add -f .
 git config user.email "git@xivic.com"
 git config user.name "xivic"
 git commit -am "[circle deploy] adding build files to specific branch"
 
-git status
-
-echo "=============================GIT PUSHING============================="
-
 # Any command that's using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
 git push ${GH_PROJECT_URL} HEAD:${GH_BRANCH_NAME} > /dev/null 2>&1
-
-echo "FINISHED"
